fa78f3c1b0d83686d2a441933167be8b5166c6dc
wei zhang <wei_zhang@g.harvard.edu>
2021-07-08 10:33:31 -0400

    Delete a face (surface)
    This measure can delete safely a face/surface either non drawn or damaged from geometry

--- measure.rb
+++ measure.rb
@@ -1,50 +1,36 @@
 # insert your copyright here
-# Wei Zhang 
-# OpenStudio Application v1.1.0
+
 # see the URL below for information on how to write OpenStudio measures
 # http://nrel.github.io/OpenStudio-user-documentation/reference/measure_writing_guide/
 
-# This measure change the openstuio::model::ConstructionWithInternalSource 
-
 # start the measure
-class TABSSlabThermal < OpenStudio::Measure::ModelMeasure
+class DeleteNondrawnFace < OpenStudio::Measure::ModelMeasure
   # human readable name
   def name
     # Measure name should be the title case of the class name.
-    return 'TABS Slab Thermal'
+    return 'Delete nondrawn face'
   end
 
   # human readable description
   def description
-    return 'Change the thermal behavior of Construction with Internal Source '
+    return 'Delete a specified face safely'
   end
 
   # human readable description of modeling approach
   def modeler_description
-    return 'Change the thermal behavior of Construction with Internal Source '
-  end
-
-  def check_multiplier(runner, multiplier)
-    if multiplier < 0
-      runner.registerError("Multiplier #{multiplier} cannot be negative.")
-      false
-    end
+    return 'Delete a specified face safely'
   end
 
   # define the arguments that the user will input
   def arguments(model)
     args = OpenStudio::Measure::OSArgumentVector.new
 
-	# make an argument insulation R-value
-    r_value_mult = OpenStudio::Measure::OSArgument.makeDoubleArgument('r_value_mult', true)
-    r_value_mult.setDisplayName('Slab total R-value multiplier')
-    r_value_mult.setDefaultValue(1)
-    args << r_value_mult
-
-    thermal_mass_mult = OpenStudio::Measure::OSArgument.makeDoubleArgument('thermal_mass_mult', true)
-    thermal_mass_mult.setDisplayName('Slab thermal mass multiplier')
-    thermal_mass_mult.setDefaultValue(1)
-    args << thermal_mass_mult
+    # the name of the space to add to the model
+    surface_name = OpenStudio::Measure::OSArgument.makeStringArgument('surface_name', true)
+    surface_name.setDisplayName('Surface name to delete')
+    surface_name.setDescription('This surface will be deleted from the geometry.')
+	surface_name.setDefaultValue('Face 216')
+    args << surface_name
 
     return args
   end
@@ -53,58 +39,46 @@ class TABSSlabThermal < OpenStudio::Measure::ModelMeasure
   def run(model, runner, user_arguments)
     super(model, runner, user_arguments)
 
-
-
     # use the built-in error checking
     if !runner.validateUserArguments(arguments(model), user_arguments)
       return false
     end
 
-    # assign the user inputs to variables : input from GUI
-    r_value_mult = runner.getDoubleArgumentValue('r_value_mult', user_arguments)
-    check_multiplier(runner, r_value_mult)
+    # assign the user inputs to variables
+    surface_name = runner.getStringArgumentValue('surface_name', user_arguments)
 
-    thermal_mass_mult = runner.getDoubleArgumentValue('thermal_mass_mult', user_arguments)
-    check_multiplier(runner, thermal_mass_mult)
+    # check the space_name for reasonableness
+    if surface_name.empty?
+      runner.registerError('Empty space name was entered.')
+      return false
+    end
 	
-	# parse openstuio::model::ConstructionWithInternalSource and deal with each construction
-	model.getConstructionWithInternalSources.each do |construction|
-	   # fetch all the layers in a construction 	   
-	   layers = construction.layers
-	   # parse each layer with index
-	   layers.each_with_index do |layer, l_index|
-	     runner.registerInfo("Name: #{layer.name}")
-		 runner.registerInfo("Handle: #{layer.handle.to_s}")
-		 
-	     runner.registerInfo("Original Thermal Resistance: #{layer.to_OpaqueMaterial.get.thermalResistance}")
-		 runner.registerInfo("Original Density: #{layer.to_StandardOpaqueMaterial.get.density}")
-		 #runner.registerInfo("Solar absorptance #{layer.to_StandardOpaqueMaterial.get.solarAbsorptance}")
-		 ro_t = layer.to_OpaqueMaterial.get.thermalResistance
-		 ro_d = layer.to_StandardOpaqueMaterial.get.density
-		 
-		 # copy and conversion 
-		 new_layer = layer.clone
-         new_layer = new_layer.to_Material.get
-		 
-		 # set the thermal parameters and layer name
-		 new_layer.to_OpaqueMaterial.get.setThermalResistance(ro_t*r_value_mult)
-		 new_layer.to_StandardOpaqueMaterial.get.setDensity(ro_d*thermal_mass_mult)
-		 new_layer.setName("#{layer.name} (R:#{r_value_mult},D:#{thermal_mass_mult})")
-		 
-		 # update construction with new layers
-		 construction.setLayer(l_index, new_layer)
-		 runner.registerInfo("Updated Layer Name: #{new_layer.name}")
-		 runner.registerInfo("Updated Thermal Resistance: #{new_layer.to_OpaqueMaterial.get.thermalResistance}")
-		 runner.registerInfo("Updated Density: #{new_layer.to_StandardOpaqueMaterial.get.density}")
-	   end 
-	   runner.registerInfo("The modified construction is: #{construction}")
-	end	
+	surfaces = model.getSurfaces
+	surface_delete = ''
+	# report initial condition of model
+    runner.registerInitialCondition("The building started with #{surfaces.size} surfaces.")
+	
+	surfaces.each do |surface|
+	  
+	  if surface.name.get.match(surface_name.to_s)
+	     #surface.remove()
+		 surface_delete = surface.name
+		 runner.registerInfo("we found #{surface.name}.")
+		 surface.remove()
+	  
+	  end
+	end 
 
 
+    # echo the new space's name back to the user
+    runner.registerInfo("Surface #{surface_delete} was removed.")
+
+    # report final condition of model
+    runner.registerFinalCondition("The building finished with #{model.getSurfaces.size} surfaces.")
 
     return true
   end
 end
 
 # register the measure to be used by the application
-TABSSlabThermal.new.registerWithApplication
+DeleteNondrawnFace.new.registerWithApplication
